622c4de2c374e9e4d56bd9849f8159618a80fd68
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-05-11 02:59:12 +0200

    CIFAR-100 updated

--- train_three_sets.sh
+++ train_three_sets.sh
@@ -72,13 +72,13 @@ for noise_level in 20 25 30 35 40 45; do
     # # ### MixUp with hard bootsraping and probes for noisy example identification (including high regularization)
     # for lambda in -1.0 -0.5 -0.25 0.0 0.25 0.5 1.0; do
     for lambda in -0.5 0.5; do
-        job=cifar10_noise_${noise_level}_m_dyr_h_three_sets_random_probes_pretraining_stop_0.2_noisy_std_$lambda
+        job=cifar100_noise_${noise_level}_m_dyr_h_three_sets_random_probes_pretraining_stop_0.2_noisy_std_$lambda
         srun -p RTXA6000 -K -N1 --ntasks-per-node=1 --gpus-per-task=1 --cpus-per-gpu=4 --mem=24G \
             --kill-on-bad-exit --job-name ${job} --nice=0 \
             --container-mounts=/netscratch:/netscratch,/ds:/ds,/home/siddiqui:/home/siddiqui --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_21.06-py3.sqsh \
             --container-workdir=`pwd` --container-mount-home --export="NCCL_SOCKET_IFNAME=bond,NCCL_IB_HCA=mlx5" \
-            /opt/conda/bin/python /netscratch/siddiqui/Repositories/LabelNoiseCorrection/train_three_subs.py --Mixup 'Static' --experiment-name "CIFAR10-M-DYR-H-Three-Sets-Random-Probes-Pretraining-Stop-0.2-Noisy-Std_"$lambda --BootBeta "HardProbes" \
-                --epochs 300 --M 100 250 --noise-level ${noise_level} --reg-term 1.0 --dataset CIFAR10 --root-dir /netscratch/siddiqui/Repositories/data/cifar10/ --use-three-sets \
+            /opt/conda/bin/python /netscratch/siddiqui/Repositories/LabelNoiseCorrection/train_three_subs.py --Mixup 'Static' --experiment-name "CIFAR100-M-DYR-H-Three-Sets-Random-Probes-Pretraining-Stop-0.2-Noisy-Std_"$lambda --BootBeta "HardProbes" \
+                --epochs 300 --M 100 250 --noise-level ${noise_level} --reg-term 1.0 --dataset CIFAR100 --root-dir /netscratch/siddiqui/Repositories/data/cifar100/ --use-three-sets \
                 --bootstrap-probe-acc-thresh 20.0 --use-probes-for-pretraining --std-lambda $lambda > \
                 /netscratch/siddiqui/Repositories/LabelNoiseCorrection/logs_three_sets/${job}.log 2>&1 &
     done
