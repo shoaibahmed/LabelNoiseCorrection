74b37da438fff6d34dd13a214913c679c0669406
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2022-06-06 14:36:55 +0200

    Plotting script updated with error bars

--- plot_data.py
+++ plot_data.py
@@ -25,57 +25,65 @@ assert len(model_dirs) == len(model_name_list)
 
 all_results = {}
 is_cifar100 = None
-for i, model_dir in enumerate(model_dirs):
-    if model_dir.startswith("#"):
-        print("Ignoring commented line:", model_dir)
+for i, main_model_dir in enumerate(model_dirs):
+    if main_model_dir.startswith("#"):
+        print("Ignoring commented line:", main_model_dir)
         continue
-    assert os.path.exists(model_dir), model_dir
-    print("Loading model directory:", model_dir)
-    models = glob(os.path.join(model_dir, "**/last_epoch_*.pth"), recursive=True)
-    print(models)
+    current_model_dir = main_model_dir.split(";")
+    print("Model directories with seeds:", current_model_dir)
     
-    results = {}
-    for model in models:
-        model_name = os.path.splitext(os.path.split(model)[-1])[0]
-        model_name_parts = model_name.split("_")
+    for model_dir in current_model_dir:
+        assert os.path.exists(model_dir), model_dir
+        print("Loading model directory:", model_dir)
+        models = glob(os.path.join(model_dir, "**/last_epoch_*.pth"), recursive=True)
+        print(models)
         
-        assert model_name_parts[5] == "valAcc"
-        val_acc = float(model_name_parts[6])
-        assert 0. <= val_acc <= 100.
+        results = {}
+        for model in models:
+            model_name = os.path.splitext(os.path.split(model)[-1])[0]
+            model_name_parts = model_name.split("_")
+            
+            assert model_name_parts[5] == "valAcc"
+            val_acc = float(model_name_parts[6])
+            assert 0. <= val_acc <= 100.
+            
+            assert model_name_parts[7] == "noise"
+            noise_level = float(model_name_parts[8])
+            assert noise_level in [float(x) for x in range(0, 100, 1)]
+            
+            assert model_name_parts[9] == "bestValLoss"
+            best_val_acc = float(model_name_parts[10])
+            assert 0. <= best_val_acc <= 100.
+            
+            print(f"Noise level: {noise_level} / Val acc: {val_acc} / Best val acc: {best_val_acc}")
+            results[noise_level] = (val_acc, best_val_acc)
         
-        assert model_name_parts[7] == "noise"
-        noise_level = float(model_name_parts[8])
-        assert noise_level in [float(x) for x in range(0, 100, 1)]
+        if is_cifar100 is None:
+            is_cifar100 = 'cifar100' in model_dir.lower()
         
-        assert model_name_parts[9] == "bestValLoss"
-        best_val_acc = float(model_name_parts[10])
-        assert 0. <= best_val_acc <= 100.
-        
-        print(f"Noise level: {noise_level} / Val acc: {val_acc} / Best val acc: {best_val_acc}")
-        results[noise_level] = (val_acc, best_val_acc)
-    
-    # dir_name = os.path.split(model_dir)[-1].replace("noise_models_PreResNet18_", "").replace("./", "").replace("/", "")
-    # dir_name = model_dir.replace("noise_models_PreResNet18_", "").replace("./", "").replace("/", "")
-    if is_cifar100 is None:
-        is_cifar100 = 'cifar100' in model_dir.lower()
-    
-    dir_name = model_name_list[i]
-    print(dir_name)
-    all_results[dir_name] = results
+        dir_name = model_name_list[i]
+        print(dir_name)
+        if dir_name in all_results:
+            for noise_level in results:
+                all_results[dir_name][noise_level].append(results[noise_level])
+        else:
+            all_results[dir_name] = {}
+            for noise_level in results:
+                all_results[dir_name][noise_level] = [results[noise_level]]
 print(all_results)
 
 # Add other baseline results
-results_cifar10 = {"Reed et al. (2015)": {0.0: (94.6, 94.7), 20.0: (82.9, 86.8), 50.0: (58.4, 79.8), 80.0: (26.8, 63.3), 90.0: (17.0, 42.9)},
-                   "Patrini et al. (2017)": {0.0: (94.6, 94.7), 20.0: (83.1, 86.8), 50.0: (59.4, 79.8), 80.0: (26.2, 63.3), 90.0: (18.8, 42.9)},
-                   "Zhang et al. (2018)": {0.0: (95.2, 95.3), 20.0: (92.3, 95.6), 50.0: (77.6, 87.1), 80.0: (46.7, 71.6), 90.0: (43.9, 52.2)},
-                   "Arazo et al. (M-DYR-H) (2019)": {0.0: (93.4, 93.6), 20.0: (93.8, 94.0), 50.0: (91.9, 92.0), 80.0: (86.6, 86.8), 90.0: (9.9, 40.8)},}
-                #    "Arazo et al. (MD-DYR-SH) (2019)": {0.0: (92.7, 93.6), 20.0: (93.6, 93.8), 50.0: (90.3, 90.6), 80.0: (77.8, 82.4), 90.0: (68.7, 69.1)}}
+results_cifar10 = {"Reed et al. (2015)": {0.0: [(94.6, 94.7)], 20.0: [(82.9, 86.8)], 50.0: [(58.4, 79.8)], 80.0: [(26.8, 63.3)], 90.0: [(17.0, 42.9)]},
+                   "Patrini et al. (2017)": {0.0: [(94.6, 94.7)], 20.0: [(83.1, 86.8)], 50.0: [(59.4, 79.8)], 80.0: [(26.2, 63.3)], 90.0: [(18.8, 42.9)]},
+                   "Zhang et al. (2018)": {0.0: [(95.2, 95.3)], 20.0: [(92.3, 95.6)], 50.0: [(77.6, 87.1)], 80.0: [(46.7, 71.6)], 90.0: [(43.9, 52.2)]},}
+                #    "Arazo et al. (M-DYR-H) (2019)": {0.0: [(93.4, 93.6)], 20.0: [(93.8, 94.0)], 50.0: [(91.9, 92.0)], 80.0: [(86.6, 86.8)], 90.0: [(9.9, 40.8)]},}
+                #    "Arazo et al. (MD-DYR-SH) (2019)": {0.0: [(92.7, 93.6)], 20.0: [(93.6, 93.8)], 50.0: [(90.3, 90.6)], 80.0: [(77.8, 82.4)], 90.0: [(68.7, 69.1)]}}
 
-results_cifar100 = {"Reed et al. (2015)": {0.0: (75.9, 76.1), 20.0: (62.0, 62.1), 50.0: (37.9, 46.6), 80.0: (8.9, 19.9), 90.0: (3.8, 10.2)},
-                   "Patrini et al. (2017)": {0.0: (75.2, 75.4), 20.0: (61.4, 61.5), 50.0: (37.3, 46.6), 80.0: (9.0, 19.9), 90.0: (3.4, 10.2)},
-                   "Zhang et al. (2018)": {0.0: (74.4, 74.8), 20.0: (66.0, 67.8), 50.0: (46.6, 57.3), 80.0: (17.6, 30.8), 90.0: (8.1, 14.6)},
-                   "Arazo et al. (M-DYR-H) (2019)": {0.0: (66.2, 70.3), 20.0: (68.5, 68.7), 50.0: (58.8, 61.7), 80.0: (47.6, 48.2), 90.0: (8.6, 12.5)},}
-                #    "Arazo et al. (MD-DYR-SH) (2019)": {0.0: (71.3, 73.3), 20.0: (73.4, 73.9), 50.0: (65.4, 66.1), 80.0: (35.4, 41.6), 90.0: (20.5, 24.3)}}
+results_cifar100 = {"Reed et al. (2015)": {0.0: [(75.9, 76.1)], 20.0: [(62.0, 62.1)], 50.0: [(37.9, 46.6)], 80.0: [(8.9, 19.9)], 90.0: [(3.8, 10.2)]},
+                   "Patrini et al. (2017)": {0.0: [(75.2, 75.4)], 20.0: [(61.4, 61.5)], 50.0: [(37.3, 46.6)], 80.0: [(9.0, 19.9)], 90.0: [(3.4, 10.2)]},
+                   "Zhang et al. (2018)": {0.0: [(74.4, 74.8)], 20.0: [(66.0, 67.8)], 50.0: [(46.6, 57.3)], 80.0: [(17.6, 30.8)], 90.0: [(8.1, 14.6)]},}
+                #    "Arazo et al. (M-DYR-H) (2019)": {0.0: [(66.2, 70.3)], 20.0: [(68.5, 68.7)], 50.0: [(58.8, 61.7)], 80.0: [(47.6, 48.2)], 90.0: [(8.6, 12.5)]},}
+                #    "Arazo et al. (MD-DYR-SH) (2019)": {0.0: [(71.3, 73.3)], 20.0: [(73.4, 73.9)], 50.0: [(65.4, 66.1)], 80.0: [(35.4, 41.6)], 90.0: [(20.5, 24.3)]}}
 
 model_types = list(all_results.keys())
 noise_levels = []
@@ -111,21 +119,29 @@ else:
 for idx in range(len(model_types)):
     for plot_best in [False] if plot_end_acc else [True]:
         model_results = all_results[model_types[idx]]
-        acc_list = []
+        mean_acc_list = []
         noise_list = []
+        std_acc_list = []
         for noise_level in noise_levels:
             if noise_level not in model_results:
                 print(f"Warning: Noise level ({noise_level}) results not found for model: {model_types[idx]}")
                 continue
-            acc_list.append(model_results[noise_level][1 if plot_best else 0])
+            assert isinstance(model_results[noise_level], list), model_results[noise_level]
+            accs = [model_results[noise_level][i][1 if plot_best else 0] for i in range(len(model_results[noise_level]))]
+            mean_acc = np.mean(accs)
+            std_acc = np.std(accs)
+            print(f"Model type: {model_types[idx]} / Noise level: {noise_level} / Acc list: {accs} / Mean: {mean_acc} / Std acc: {std_acc}")
+            mean_acc_list.append(mean_acc)
+            std_acc_list.append(std_acc)
             noise_list.append(noise_level)
         
-        line = plt.plot(noise_list, acc_list, linewidth=4., marker=marker_list[idx % len(marker_list)],
+        mean_acc_list = np.array(mean_acc_list)
+        std_acc_list = np.array(std_acc_list)
+        
+        line = plt.plot(noise_list, mean_acc_list, linewidth=4., marker=marker_list[idx % len(marker_list)],
                         color=marker_colors[idx], alpha=0.6, markeredgecolor='k', markersize=9, label=f"{model_types[idx]}{' (Best)' if plot_best and plot_end_acc else ''}")
-        # line = plt.plot(noise_list, acc_list, linewidth=2., marker=marker_list[idx % len(marker_list)],
-        #                 color=marker_colors[idx], alpha=0.75, markeredgecolor='k', label=model_name_list[idx])
+        plt.fill_between(noise_list, mean_acc_list - std_acc_list, mean_acc_list + std_acc_list, color=marker_colors[idx], alpha=0.25)
         line[0].set_color(marker_colors[idx])
-        # line[0].set_linestyle(line_styles[(idx*2+(1 if plot_best else 0)) % len(line_styles)])
         line[0].set_linestyle(line_styles[(1 if plot_best else 0) % len(line_styles)])
 
 font_size = 16
